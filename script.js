bad162a0b8a85a1d9f4ddd49bc454b24510f1129
Venancius <steniovenancius@gmail.com>
2023-07-30 23:00:32 -0300

    Feature
    Inicio do Javascript e conclusão do CSS

---
+++ script.js
@@ -0,0 +1,19 @@
+const frontCardName = document.getElementById('front_name');
+const inputName = document.querySelector('#inputName');
+
+const frontCardNumber = document.getElementById('front_number');
+const inputNumber = document.querySelector('#inputNumber');
+
+const frontCardDataMonth = document.getElementById('front_month');
+const inputDataMonth = document.getElementById('inputMonth').value;
+
+const frontCardDataYear = document.getElementById('front_year');
+const inputDataYear = document.getElementById('inputYear').value;
+
+const backCardCvc = document.getElementById('back_cvc');
+const inputCvc = document.getElementById('inputCVC').value;
+
+function cardHolder() {
+    //recupera o que foi digitado no input e substitui no campo de nome
+    frontCardName.innerHTML = inputName.value;
+}
